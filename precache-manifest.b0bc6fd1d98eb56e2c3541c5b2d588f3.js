247d95f81c69ff06b1aca871040aeeefdea62376
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2019-10-28 20:53:08 +0530

    initial commit

---
+++ precache-manifest.b0bc6fd1d98eb56e2c3541c5b2d588f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998fff673f964a8c860e63bda09043de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af8edd32785ad90b8746",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2a5010a16d366f460c7a",
+    "url": "/static/js/2.85dae2fb.chunk.js"
+  },
+  {
+    "revision": "af8edd32785ad90b8746",
+    "url": "/static/js/main.634245b2.chunk.js"
+  },
+  {
+    "revision": "79beb54b993ed6432830",
+    "url": "/static/js/runtime-main.4585df84.js"
+  }
+]);
