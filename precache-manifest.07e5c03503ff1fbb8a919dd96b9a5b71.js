51bfe769fb581264692aad8b3facbd011085a51a
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2019-12-26 09:55:17 +0530

    removing stuff

---
+++ precache-manifest.07e5c03503ff1fbb8a919dd96b9a5b71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4e55626f9052cc58d0b2947fd29c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d60fbfb5f5d0b4573ff",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2a5010a16d366f460c7a",
+    "url": "/static/js/2.85dae2fb.chunk.js"
+  },
+  {
+    "revision": "3d60fbfb5f5d0b4573ff",
+    "url": "/static/js/main.05f00e41.chunk.js"
+  },
+  {
+    "revision": "79beb54b993ed6432830",
+    "url": "/static/js/runtime-main.4585df84.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9fed07490b6a716bb610c35123678c0.js"
+  "/precache-manifest.07e5c03503ff1fbb8a919dd96b9a5b71.js"
 );
 
 self.addEventListener('message', (event) => {
